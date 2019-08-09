d44c2a0e8832060f8dcb3b1d4c8221284a3525ae
djdarland <student@dennisdarland.com>
2019-08-08 21:56:03 -0500

    More Work on Test Scripts. Think correct now.

--- Life/Tests/approve_out-1.02.sh
+++ Life/Tests/approve_out-1.02.sh
@@ -1,16 +1,16 @@
-if test -f OUT/$1.out
+if test -f OUT/$1.out.tmp
 then
-cp OUT/$1.out REFOUT/$1.refout
+cp OUT/$1.out.tmp REFOUT/$1.refout
 else
 rm REFOUT/$1.refout
 touch REFOUT/$1.refout
 fi
-if test -f ERR/$1.err
+if test -f ERR/$1.err.tmp
 then
-cp ERR/$1.err REFERR/$1.referr
+cp ERR/$1.err.tmp REFERR/$1.referr
 else
 rm REFERR/$1.referr
 touch REFERR/$1.referr
 fi
-echo "$1 approved" >APPROVED/$1.approved
+echo "$1 approved" >APPROVED-1.02/$1.approved
 
--- Life/Tests/approve_out.sh
+++ Life/Tests/approve_out.sh
@@ -1,13 +1,13 @@
-if test -f OUT/$1.out
+if test -f OUT/$1.out.tmp
 then
-cp OUT/$1.out REFOUT_UPDATED/$1.refout
+cp OUT/$1.out.tmp REFOUT_UPDATED/$1.refout
 else
 rm REFOUT_UPDATED/$1.refout
 touch REFOUT_UPDATED/$1.refout
 fi
-if test -f ERR/$1.err
+if test -f ERR/$1.err.tmp
 then
-cp ERR/$1.err REFERR_UPDATED/$1.referr
+cp ERR/$1.err.tmp REFERR_UPDATED/$1.referr
 else
 rm REFERR_UPDATED/$1.referr
 touch REFERR_UPDATED/$1.referr
