9692a0a9e411d4f03b5933131390d296fbe4d343
djdarland <student@dennisdarland.com>
2019-08-10 15:58:07 -0500

    X Windows seems to work - need to clean out debug.

--- Life/Source/XXXX/cp_local.sh
+++ Life/Source/XXXX/cp_local.sh
@@ -2,11 +2,11 @@ if test -f ~/life_local_redo
 then
     rm -r ~/life_local_redo
 fi
-cp -r ../life_local_redo ~
+cp -r ../../life_local_redo ~
 if test -f ~/life_local_xxxx_redo2
 then
     rm -r ~/life_local_xxxx_redo2
 fi
-cp -r ../life_local_xxxx_redo2 ~
+cp -r ../../life_local_xxxx_redo2 ~
 
 
