99a1e58a5efecabec230f1ffc8b62e33c4bcf20d
djdarland <student@dennisdarland.com>
2019-08-12 18:37:48 -0500

    cd Life/life_local_xxxx_redo2/Demo wild_life_X load("life_demo")? demo? ----------------------- Everything works exceot There is some troubles with flowers. It is written with older version of x tools. On all (I think), you get fatal X error if you use close on corner instead of quit button. I was getting other fatal errors sometimes. Not sure if was close or something else. Fonts could be improved - especially for queens.

--- Life/Source/cp_local.sh
+++ Life/Source/cp_local.sh
@@ -1,9 +1,9 @@
-if test -f ~/life_local_redo
+if test -d ~/life_local_redo
 then
     rm -r ~/life_local_redo
 fi
 cp -r ../life_local_redo ~
-if test -f ~/life_local_xxxx_redo2
+if test -d ~/life_local_xxxx_redo2
 then
     rm -r ~/life_local_xxxx_redo2
 fi
